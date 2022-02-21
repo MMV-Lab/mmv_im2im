d4e0701e50bb02199f5c686ba225ac3bcbe26dbb
Sata <sai.sata@ambiomnode3.isas.de>
2022-02-21 19:37:19 +0100

    Code update to generate synthetic data for pix2pix and cycleGAN

--- script/generate_synthetic_data.py
+++ script/generate_synthetic_data.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import numpy as np
 from random import randint
 from tqdm import tqdm
-
+from PIL import Image, ImageDraw, ImageFilter
 from skimage.morphology import dilation, ball
 from skimage.util import random_noise
 from scipy.ndimage import gaussian_filter
@@ -18,12 +18,12 @@ from tifffile import imsave
 
 log = logging.getLogger()
 logging.basicConfig(
-    level=logging.INFO, format="[%(levelname)4s:%(lineno)4s %(asctime)s] %(message)s"  # noqa E501
+    level=logging.INFO,
+    format="[%(levelname)4s:%(lineno)4s %(asctime)s] %(message)s",  # noqa E501
 )
 
 
 class Args(argparse.Namespace):
-
     def __init__(self):
         # Arguments that could be passed in through the command line
         self.debug = False
@@ -43,33 +43,15 @@ class Args(argparse.Namespace):
             action="store_true",
             help="whether to generate costmap",
         )
+        p.add_argument("--type", default="mask", help="tyep of target: mask or im")
         p.add_argument(
-            "--type",
-            default="mask",
-            help="tyep of target: mask or im"
+            "--large", action="store_true", help="whether to generate large image"
         )
+        p.add_argument("--num", default=10, type=int, help="number of synthetic data")
         p.add_argument(
-            "--large",
-            action="store_true",
-            help="whether to generate large image"
+            "--dim", default=3, type=int, help="either 2 or 3 dimensional data"
         )
-        p.add_argument(
-            "--num",
-            default=10,
-            type=int,
-            help="number of synthetic data"
-        )
-        p.add_argument(
-            "--dim",
-            default=3,
-            type=int,
-            help="either 2 or 3 dimensional data"
-        )
-        p.add_argument(
-            "--out",
-            required=True,
-            help="the path to save the data"
-        )
+        p.add_argument("--out", required=True, help="the path to save the data")
         p.add_argument(
             "--debug",
             action="store_true",
@@ -96,23 +78,56 @@ def generate_data(args):
                 im = np.zeros((64, 512, 512))
             else:
                 im = np.zeros((64, 128, 128))
-            for obj in range(num_obj):
-                py = randint(15, im.shape[-2]-15)
-                px = randint(15, im.shape[-1]-15)
-                im[31, py, px] = 1
-            im = dilation(im > 0, ball(2))
-
             if args.type == "mask" and not args.unpair:
+                for obj in range(num_obj):
+                    py = randint(15, im.shape[-2] - 15)
+                    px = randint(15, im.shape[-1] - 15)
+                    im[31, py, px] = 1
+                im = dilation(im > 0, ball(2))
                 gt = im.astype(np.float32)
                 gt[gt > 0] = 1
                 imsave(out_gt_fn, gt)
+                raw = gaussian_filter(im.astype(np.float32), 5)
+                raw = random_noise(raw).astype(np.float32)
+                imsave(out_raw_fn, raw)
+            elif args.type == "im" and args.unpair:
+                x, y = (128, 128)
+                eX, eY = 60, 60
+                im_a = Image.new("L", (128, 128), 0)
+                draw = ImageDraw.Draw(im_a)
+                draw.ellipse(
+                    (x / 2 - eX / 2, y / 2 - eY / 2, x / 2 + eX / 2, y / 2 + eY / 2),
+                    fill=255,
+                )
+                im_a_blur = im_a.filter(ImageFilter.GaussianBlur(15))
+                im_GT = np.zeros((32, 128, 128))
+                im_raw = np.zeros((32, 128, 128))
+                c, _, _ = im_GT.shape
+                for channel in range(c):
+                    im_GT[channel, :, :] = im_a
+                    im_raw[channel, :, :] = im_a_blur
+                imsave(out_gt_fn, im_GT)
+                imsave(out_raw_fn, im_raw)
+            elif args.type == "im" and (not args.unpair):
+                w, h = (8, 8)
+                img = Image.new("L", (w, h), 0)
+                pixels = img.load()
+                for i in range(w):
+                    for j in range(h):
+                        if (i + j) % 2 == 0:
+                            pixels[i, j] = 255
+                img = img.resize((128, 128), Image.NEAREST)
+                im_GT = np.zeros((32, 128, 128))
+                im_raw = np.zeros((32, 128, 128))
+                c, w, h = im_GT.shape
+                for channel in range(c):
+                    im_GT[channel, :, :] = np.array(img)
+                    im_raw[channel, :, :] = np.array(img.filter(ImageFilter.FIND_EDGES))
+                imsave(out_gt_fn, im_GT.astype(np.uint8))
+                imsave(out_raw_fn, im_raw.astype(np.uint8))
             else:
-                print("Not impletemented yet")
+                print("Not implemented yet")
                 exit(0)
-
-            raw = gaussian_filter(im.astype(np.float32), 5)
-            raw = random_noise(raw).astype(np.float32)
-            imsave(out_raw_fn, raw)
 
             if args.costmap:
                 costmap = np.ones_like(raw)
