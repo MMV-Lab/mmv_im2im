d7f889b850e8e58ff669f2d5487a8a6809074323
Jianxu Chen <jxchen.ustc@gmail.com>
2022-02-24 19:40:57 +0100

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     url="https://github.com/MMV-Lab/mmv_im2im",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="0.1.0",
     zip_safe=False,
 )
