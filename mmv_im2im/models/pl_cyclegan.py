1a6f57fc5226b264325a304abce37db1f26f84cb
Jianxu Chen <jxchen.ustc@gmail.com>
2022-05-11 20:24:01 +0200

    fix data loader and model setting

--- mmv_im2im/models/pl_cyclegan.py
+++ mmv_im2im/models/pl_cyclegan.py
@@ -19,8 +19,12 @@ class Model(pl.LightningModule):
         super(Model, self).__init__()
         self.generator_model_image_AtoB = define_generator(model_info_xx["generator"])
         self.generator_model_image_BtoA = define_generator(model_info_xx["generator"])
-        self.discriminator_model_image_A = define_discriminator(model_info_xx["discriminator"])
-        self.discriminator_model_image_B = define_discriminator(model_info_xx["discriminator"])
+        self.discriminator_model_image_A = define_discriminator(
+            model_info_xx["discriminator"]
+        )
+        self.discriminator_model_image_B = define_discriminator(
+            model_info_xx["discriminator"]
+        )
         if train:
             # buffer for fake images
             if "fake_pool_size" in model_info_xx["criterion"]:
@@ -38,9 +42,9 @@ class Model(pl.LightningModule):
             )
 
             # get weights of different loss
-            self.gan_w = model_info_xx["criterion"]["gan_loss"]
-            self.cycle_w = model_info_xx["criterion"]["cycle_loss"]
-            self.identity_w = model_info_xx["criterion"]["identity_loss"]
+            self.gan_w = model_info_xx["criterion"]["weights"]["gan_loss"]
+            self.cycle_w = model_info_xx["criterion"]["weights"]["cycle_loss"]
+            self.identity_w = model_info_xx["criterion"]["weights"]["identity_loss"]
 
             # get info of optimizer and scheduler
             self.optimizer_info = model_info_xx["optimizer"]
@@ -49,6 +53,23 @@ class Model(pl.LightningModule):
             self.direction = model_info_xx["direction"]
 
     def forward(self, x):
+        #  ##########################################
+        # check if the data is 2D or 3D
+        ##########################################
+        # torchio will add dummy dimension to 2D images to ensure 4D tensor
+        # see: https://github.com/fepegar/torchio/blob/1c217d8716bf42051e91487ece82f0372b59d903/torchio/data/io.py#L402  # noqa E501
+        # but in PyTorch, we usually follow the convention as C x D x H x W
+        # or C x Z x Y x X, where the Z dimension of depth dimension is before
+        # HW or YX. Padding the dummy dimmension at the end is okay and
+        # actually makes data augmentation easier to implement. For example,
+        # you have 2D image of Y x X, then becomes 1 x Y x X x 1. If you want
+        # to apply a flip on the first dimension of your image, i.e. Y, you
+        # can simply specify axes as [0], which is compatable
+        # with the syntax for fliping along Y in 1 x Y x X x 1.
+        # But, the FCN models do not like this. We just need to remove the
+        # dummy dimension
+        if x.size()[-1] == 1:
+            x = torch.squeeze(x, dim=-1)
         if self.direction == "AtoB":
             return self.generator_model_image_AtoB(x)
         elif self.direction == "BtoA":
@@ -90,31 +111,93 @@ class Model(pl.LightningModule):
         image_A = batch["source"][tio.DATA]
         image_B = batch["target"][tio.DATA]
 
+        if image_A.size()[-1] == 1:
+            image_A = torch.squeeze(image_A, dim=-1)
+            image_B = torch.squeeze(image_B, dim=-1)
+
+        # run generators and discriminators
+        fake_B_from_A = self.generator_model_image_AtoB(image_A)
+        fake_A_from_B = self.generator_model_image_BtoA(image_B)
+
+        predFakeA = self.discriminator_model_image_A(fake_A_from_B)
+        predFakeB = self.discriminator_model_image_B(fake_B_from_A)
+
+        pred_real = torch.ones_like(predFakeA, requires_grad=False)
+        pred_fake = torch.zeros_like(predFakeA, requires_grad=False)
+
+        # GAN loss
+        gan_loss_B = self.gan_loss(
+            predFakeB, pred_real
+        )
+        gan_loss_A = self.gan_loss(
+            predFakeA, pred_real
+        )
+        gan_loss = (gan_loss_A + gan_loss_B) / 2
+
+        # cycle loss
+        cycle_loss_A = self.cycle_loss(
+            self.generator_model_image_BtoA(fake_B_from_A), image_A
+        )
+        cycle_loss_B = self.cycle_loss(
+            self.generator_model_image_AtoB(fake_A_from_B), image_B
+        )
+        cycle_loss = (cycle_loss_A + cycle_loss_B) / 2
+
+        # identity loss
+        fake_A_from_A = self.generator_model_image_BtoA(image_A)
+        fake_B_from_B = self.generator_model_image_BtoA(image_B)
+        identity_loss_A = self.identity_loss(fake_A_from_A, image_A)
+        identity_loss_B = self.identity_loss(fake_B_from_B, image_B)
+        identity_loss = (identity_loss_A + identity_loss_B) / 2
+
+        # weighted sum into generator loss
+        G_loss = (
+            gan_loss * self.gan_w
+            + cycle_loss * self.cycle_w
+            + identity_loss * self.identity_w
+        )
+
+        # discriminator on domain A
+        predRealA = self.discriminator_model_image_A(image_A)
+        dis_loss_A_real = self.gan_loss(predRealA, pred_real)
+        dis_loss_A_fake = self.gan_loss(predFakeA, pred_fake)
+
+        # discriminator on domain B
+        predRealB = self.discriminator_model_image_B(image_B)
+        dis_loss_B_real = self.gan_loss(predRealB, pred_real)
+        dis_loss_B_fake = self.gan_loss(predFakeB, pred_fake)
+
+        # average over all dis loss
+        D_loss = 0.5 * (
+            dis_loss_A_fake + dis_loss_A_real + dis_loss_B_fake + dis_loss_B_real
+        )
+        """
         # generator A to B: A --> fake B
         fake_image_B = self.generator_model_image_AtoB(image_A)
         D_B_real = self.discriminator_model_image_B(image_B)
         D_B_fake = self.discriminator_model_image_B(fake_image_B.detach())
-        D_B_real_loss = self.MSE_criterion(D_B_real, torch.ones_like(D_B_real))
-        D_B_fake_loss = self.MSE_criterion(D_B_fake, torch.zeros_like(D_B_fake))
+        D_B_real_loss = self.gan_loss(D_B_real, torch.ones_like(D_B_real))
+        D_B_fake_loss = self.gan_loss(D_B_fake, torch.zeros_like(D_B_fake))
         D_loss = D_B_real_loss + D_B_fake_loss
-        loss_G_B = self.MSE_criterion(D_B_fake, torch.ones_like(D_B_fake))
+        loss_G_B = self.gan_loss(D_B_fake, torch.ones_like(D_B_fake))
         validity_loss = loss_G_B
 
         # generator B to A: fake B --> reconstructed A
         cycle_A = self.generator_model_image_BtoA(fake_image_B)
-        cycle_A_loss = self.L1_criterion(image_A, cycle_A)
+        cycle_A_loss = self.cycle_loss(image_A, cycle_A)
         cycle_loss = cycle_A_loss
 
         # identity regularization;
         # https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/issues/322
         identity_A = self.generator_model_image_BtoA(image_A)
         identity_B = self.generator_model_image_AtoB(image_B)
-        identity_A_loss = self.L1_criterion(image_A, identity_A)
-        identity_B_loss = self.L1_criterion(image_B, identity_B)
+        identity_A_loss = self.identity_loss(image_A, identity_A)
+        identity_B_loss = self.identity_loss(image_B, identity_B)
         identity_loss = (identity_A_loss + identity_B_loss) / 2
 
         # combine the loss
-        G_loss = validity_loss + cycle_loss * self.cycle_w + identity_loss * self.id_w
+        G_loss = validity_loss * self.gan_w + cycle_loss * self.cycle_w + identity_loss * self.identity_w
+        """
         output = OrderedDict({"generator_loss": G_loss, "discriminator_loss": D_loss})
         return output
 
@@ -122,6 +205,10 @@ class Model(pl.LightningModule):
         # get the batch
         image_A = batch["source"][tio.DATA]
         image_B = batch["target"][tio.DATA]
+
+        if image_A.size()[-1] == 1:
+            image_A = torch.squeeze(image_A, dim=-1)
+            image_B = torch.squeeze(image_B, dim=-1)
 
         # generate fake images
         fake_A_from_B = self.generator_model_image_BtoA(image_B)
@@ -132,12 +219,16 @@ class Model(pl.LightningModule):
             # train generators
             ############################
             # GAN loss
-            gan_valid = torch.ones_like(image_A, requires_grad=False)
+            predFakeA = self.discriminator_model_image_A(fake_A_from_B)
+            predFakeB = self.discriminator_model_image_B(fake_B_from_A)
+
+            pred_real = torch.ones_like(predFakeA, requires_grad=False)
+
             gan_loss_B = self.gan_loss(
-                self.discriminator_model_image_B(fake_B_from_A), gan_valid
+                predFakeB, pred_real
             )
             gan_loss_A = self.gan_loss(
-                self.discriminator_model_image_A(fake_A_from_B), gan_valid
+                predFakeA, pred_real
             )
             gan_loss = (gan_loss_A + gan_loss_B) / 2
 
@@ -179,24 +270,22 @@ class Model(pl.LightningModule):
             ############################
             # train discriminators
             ############################
-
-            pred_real = torch.ones_like(image_A, requires_grad=False)
-            pred_fake = torch.zeros_like(image_A, requires_grad=False)
             fakeA_sample = self.fake_pool_A.push_and_pop(fake_A_from_B)
             fakeB_sample = self.fake_pool_B.push_and_pop(fake_B_from_A)
+            predFakeA = self.discriminator_model_image_A(fakeA_sample)
+            predFakeB = self.discriminator_model_image_B(fakeB_sample)
 
+            pred_real = torch.ones_like(predFakeA, requires_grad=False)
+            pred_fake = torch.zeros_like(predFakeA, requires_grad=False)
+            
             # discriminator on domain A
             predRealA = self.discriminator_model_image_A(image_A)
             dis_loss_A_real = self.gan_loss(predRealA, pred_real)
-
-            predFakeA = self.discriminator_model_image_A(fakeA_sample)
             dis_loss_A_fake = self.gan_loss(predFakeA, pred_fake)
 
             # discriminator on domain B
             predRealB = self.discriminator_model_image_B(image_B)
             dis_loss_B_real = self.gan_loss(predRealB, pred_real)
-
-            predFakeB = self.discriminator_model_image_B(fakeB_sample)
             dis_loss_B_fake = self.gan_loss(predFakeB, pred_fake)
 
             # average over all dis loss
