70f1cfdff3b771f6b870afd672e48a2be1de0f31
Jianxu Chen <jxchen.ustc@gmail.com>
2022-05-17 22:33:43 +0200

    minor cleanup

--- mmv_im2im/models/pl_pix2pix.py
+++ mmv_im2im/models/pl_pix2pix.py
@@ -15,7 +15,6 @@ from tifffile import imsave
 from functools import partial
 
 
-"""
 def weights_init(module):
     if isinstance(module, nn.Conv2d):
         module.weight.detach().normal_(0.0, 0.02)
@@ -24,87 +23,7 @@ def weights_init(module):
         module.weight.detach().normal_(1.0, 0.02)
         module.bias.detach().fill_(0.0)
 
-# [2] Set the Normalization method for the input layer
-
-
-def get_norm_layer(type):
-    if type == 'BatchNorm2d':
-        layer = partial(nn.BatchNorm2d, affine=True)
-
-    elif type == 'InstanceNorm2d':
-        layer = partial(nn.InstanceNorm2d, affine=False)
-
-    return layer
-
-# [3] Set the Padding method for the input layer
-
-
-def get_pad_layer(type):
-    if type == 'reflection':
-        layer = nn.ReflectionPad2d
-
-    elif type == 'replication':
-        layer = nn.ReplicationPad2d
-
-    elif type == 'zero':
-        layer = nn.ZeroPad2d
-
-    else:
-        raise NotImplementedError("Padding type {} is not valid."
-                                  " Please choose among ['reflection', 'replication', 'zero']".format(type))
-
-    return layer
-
-
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-
-        act = nn.ReLU(inplace=True)
-        input_ch = 1  # opt.input_ch
-        n_gf = 64  # opt.n_gf
-        norm = get_norm_layer('InstanceNorm2d')  # opt.norm_type
-        output_ch = 1  # opt.target_ch
-        pad = get_pad_layer('reflection')  # opt.padding_type
-
-        model = []
-        model += [pad(3), nn.Conv2d(input_ch, n_gf, kernel_size=7, padding=0), norm(n_gf), act]
-
-        for _ in range(4):  # opt.n_downsample
-            model += [nn.Conv2d(n_gf, 2 * n_gf, kernel_size=3, padding=1, stride=2), norm(2 * n_gf), act]
-            n_gf *= 2
-
-        for _ in range(9):  # opt.n_residual
-            model += [ResidualBlock(n_gf, pad, norm, act)]
-
-        for _ in range(4):  # opt.n_downsample
-            model += [nn.ConvTranspose2d(n_gf, n_gf // 2, kernel_size=3, padding=1, stride=2, output_padding=1),
-                      norm(n_gf // 2), act]
-            n_gf //= 2
-
-        model += [pad(3), nn.Conv2d(n_gf, output_ch, kernel_size=7, padding=0)]
-        self.model = nn.Sequential(*model)
-
-        print(self)
-        print("the number of G parameters", sum(p.numel() for p in self.parameters() if p.requires_grad))
-
-    def forward(self, x):
-        return self.model(x)
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, n_channels, pad, norm, act):
-        super(ResidualBlock, self).__init__()
-        block = [pad(1), nn.Conv2d(n_channels, n_channels, kernel_size=3, padding=0, stride=1), norm(n_channels), act]
-        block += [pad(1), nn.Conv2d(n_channels, n_channels, kernel_size=3, padding=0, stride=1), norm(n_channels)]
-        self.block = nn.Sequential(*block)
-
-    def forward(self, x):
-        return x + self.block(x)
-
-# [2] Discriminative Network
-
-
+"""
 class PatchDiscriminator(nn.Module):
     def __init__(self):
         super(PatchDiscriminator, self).__init__()
@@ -199,146 +118,6 @@ class Loss(object):
         return loss_D, loss_G, target, fake
 
 
-"""
-class DownSampleConv(nn.Module):
-
-    def __init__(self, in_channels, out_channels, kernel=4, strides=2, padding=1, activation=True, batchnorm=True):
-        super().__init__()
-        self.activation = activation
-        self.batchnorm = batchnorm
-
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel, strides, padding)
-
-        if batchnorm:
-            self.bn = nn.BatchNorm2d(out_channels)
-
-        if activation:
-            self.act = nn.LeakyReLU(0.2)
-
-    def forward(self, x):
-        x = self.conv(x)
-        if self.batchnorm:
-            x = self.bn(x)
-        if self.activation:
-            x = self.act(x)
-        return x
-
-
-class UpSampleConv(nn.Module):
-
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel=4,
-        strides=2,
-        padding=1,
-        activation=True,
-        batchnorm=True,
-        dropout=False
-    ):
-        super().__init__()
-        self.activation = activation
-        self.batchnorm = batchnorm
-        self.dropout = dropout
-
-        self.deconv = nn.ConvTranspose2d(in_channels, out_channels, kernel, strides, padding)
-
-        if batchnorm:
-            self.bn = nn.BatchNorm2d(out_channels)
-
-        if activation:
-            self.act = nn.ReLU(True)
-
-        if dropout:
-            self.drop = nn.Dropout2d(0.5)
-
-    def forward(self, x):
-        x = self.deconv(x)
-        if self.batchnorm:
-            x = self.bn(x)
-
-        if self.dropout:
-            x = self.drop(x)
-        return x
-
-
-class Generator(nn.Module):
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-
-        # encoder/donwsample convs
-        self.encoders = [
-            DownSampleConv(in_channels, 64, batchnorm=False),  # bs x 64 x 128 x 128
-            DownSampleConv(64, 128),  # bs x 128 x 64 x 64
-            DownSampleConv(128, 256),  # bs x 256 x 32 x 32
-            DownSampleConv(256, 512),  # bs x 512 x 16 x 16
-            DownSampleConv(512, 512),  # bs x 512 x 8 x 8
-            DownSampleConv(512, 512),  # bs x 512 x 4 x 4
-            DownSampleConv(512, 512),  # bs x 512 x 2 x 2
-            DownSampleConv(512, 512, batchnorm=False),  # bs x 512 x 1 x 1
-        ]
-
-        # decoder/upsample convs
-        self.decoders = [
-            UpSampleConv(512, 512, dropout=True),  # bs x 512 x 2 x 2
-            UpSampleConv(1024, 512, dropout=True),  # bs x 512 x 4 x 4
-            UpSampleConv(1024, 512, dropout=True),  # bs x 512 x 8 x 8
-            UpSampleConv(1024, 512),  # bs x 512 x 16 x 16
-            UpSampleConv(1024, 256),  # bs x 256 x 32 x 32
-            UpSampleConv(512, 128),  # bs x 128 x 64 x 64
-            UpSampleConv(256, 64),  # bs x 64 x 128 x 128
-        ]
-        self.decoder_channels = [512, 512, 512, 512, 256, 128, 64]
-        self.final_conv = nn.ConvTranspose2d(64, out_channels, kernel_size=4, stride=2, padding=1)
-        self.tanh = nn.Tanh()
-
-        self.encoders = nn.ModuleList(self.encoders)
-        self.decoders = nn.ModuleList(self.decoders)
-
-    def forward(self, x):
-        skips_cons = []
-        for encoder in self.encoders:
-            x = encoder(x)
-
-            skips_cons.append(x)
-
-        skips_cons = list(reversed(skips_cons[:-1]))
-        decoders = self.decoders[:-1]
-
-        for decoder, skip in zip(decoders, skips_cons):
-            x = decoder(x)
-            # print(x.shape, skip.shape)
-            x = torch.cat((x, skip), axis=1)
-
-        x = self.decoders[-1](x)
-        # print(x.shape)
-        x = self.final_conv(x)
-        return self.tanh(x)
-
-
-class PatchGAN(nn.Module):
-
-    def __init__(self, input_channels):
-        super().__init__()
-        self.d1 = DownSampleConv(input_channels, 64, batchnorm=False)
-        self.d2 = DownSampleConv(64, 128)
-        self.d3 = DownSampleConv(128, 256)
-        self.d4 = DownSampleConv(256, 512)
-        self.final = nn.Conv2d(512, 1, kernel_size=1)
-
-    def forward(self, x, y):
-        x = torch.cat([x, y], axis=1)
-        x0 = self.d1(x)
-        x1 = self.d2(x0)
-        x2 = self.d3(x1)
-        x3 = self.d4(x2)
-        xn = self.final(x3)
-        return xn
-"""
-
-
 class Model(pl.LightningModule):
     def __init__(self, model_info_xx: Dict, train: bool = True, verbose: bool = False):
         super(Model, self).__init__()
@@ -354,8 +133,8 @@ class Model(pl.LightningModule):
         # self.sliding_window = model_info_xx["sliding_window_params"]
         if train:
             # initialize model weights
-            # init_weights(self.generator)
-            # init_weights(self.discriminator)
+            init_weights(self.generator, init_type='normal')
+            init_weights(self.discriminator, init_type='normal')
 
             # get loss functions
             self.loss_func = Loss()
