6a8bf0ccfb9033159a7eeb031377ffdf9f993334
Jianxu Chen <jxchen.ustc@gmail.com>
2022-08-21 22:33:31 +0200

    add stain normalizaiton

--- mmv_im2im/preprocessing/transforms.py
+++ mmv_im2im/preprocessing/transforms.py
@@ -190,3 +190,112 @@ def pad_z(img, target_size: int = 64, pad_value: Union[str, float, int] = 0):
             return F.pad(img, pad_shape, "constant", pad_value)
     else:
         return img
+
+
+def normalize_staining(
+    img, Io=240, alpha=1, beta=0.15, return_unmix_results: bool = False
+):
+    """Normalize staining appearence of H&E stained images
+
+    Input:
+        I: RGB input image (we assume channel order CYX)
+        Io: (optional) transmitted light intensity
+        alpha: parameter from paper (see Reference)
+        beta: parameter from paper (see Reference)
+        return_unmix_results: whether to return unmixed H and E channel for debugging
+
+    Output:
+        Inorm: normalized image
+        H: (optional, only when return_unmix_results=True) hematoxylin image
+        E: (optional, only when return_unmix_results=True) eosin image
+
+    Reference:
+        A method for normalizing histology slides for quantitative analysis. M.
+        Macenko et al., ISBI 2009
+    """
+
+    HERef = np.array([[0.5626, 0.2159], [0.7201, 0.8012], [0.4062, 0.5581]])
+
+    maxCRef = np.array([1.9705, 1.0308])
+
+    # define height and width of image and move the color dimenstion to the last
+    c, h, w = img.shape
+    img = np.moveaxis(img, 0, -1)
+
+    assert c == 3, "the first dimension of the image must be 3 (RGB)"
+
+    # reshape image
+    img = img.reshape((-1, 3))
+
+    # calculate optical density
+    OD = -np.log((img.astype(np.float) + 1) / Io)
+
+    # remove transparent pixels
+    ODhat = OD[~np.any(OD < beta, axis=1)]
+
+    # compute eigenvectors
+    eigvals, eigvecs = np.linalg.eigh(np.cov(ODhat.T))
+
+    # eigvecs *= -1
+
+    # project on the plane spanned by the eigenvectors corresponding to the two
+    # largest eigenvalues
+    That = ODhat.dot(eigvecs[:, 1:3])
+
+    phi = np.arctan2(That[:, 1], That[:, 0])
+
+    minPhi = np.percentile(phi, alpha)
+    maxPhi = np.percentile(phi, 100 - alpha)
+
+    vMin = eigvecs[:, 1:3].dot(np.array([(np.cos(minPhi), np.sin(minPhi))]).T)
+    vMax = eigvecs[:, 1:3].dot(np.array([(np.cos(maxPhi), np.sin(maxPhi))]).T)
+
+    # a heuristic to make the vector corresponding to hematoxylin first and the
+    # one corresponding to eosin second
+    if vMin[0] > vMax[0]:
+        HE = np.array((vMin[:, 0], vMax[:, 0])).T
+    else:
+        HE = np.array((vMax[:, 0], vMin[:, 0])).T
+
+    # rows correspond to channels (RGB), columns to OD values
+    Y = np.reshape(OD, (-1, 3)).T
+
+    # determine concentrations of the individual stains
+    C = np.linalg.lstsq(HE, Y, rcond=None)[0]
+
+    # normalize stain concentrations
+    maxC = np.array([np.percentile(C[0, :], 99), np.percentile(C[1, :], 99)])
+    tmp = np.divide(maxC, maxCRef)
+    C2 = np.divide(C, tmp[:, np.newaxis])
+
+    # recreate the image using reference mixing matrix
+    Inorm = np.multiply(Io, np.exp(-HERef.dot(C2)))
+    Inorm[Inorm > 255] = 254
+    Inorm = np.reshape(Inorm.T, (h, w, 3)).astype(np.uint8)
+
+    # unmix hematoxylin and eosin
+    H = np.multiply(
+        Io,
+        np.exp(
+            np.expand_dims(-HERef[:, 0], axis=1).dot(np.expand_dims(C2[0, :], axis=0))
+        ),
+    )
+    H[H > 255] = 254
+    H = np.reshape(H.T, (h, w, 3)).astype(np.uint8)
+
+    E = np.multiply(
+        Io,
+        np.exp(
+            np.expand_dims(-HERef[:, 1], axis=1).dot(np.expand_dims(C2[1, :], axis=0))
+        ),
+    )
+    E[E > 255] = 254
+    E = np.reshape(E.T, (h, w, 3)).astype(np.uint8)
+
+    # move the color dimension to the first to be consistent with others transforms
+    Inorm = np.moveaxis(Inorm, -1, 0)
+
+    if return_unmix_results:
+        return Inorm, H, E
+    else:
+        return Inorm
