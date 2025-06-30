861b155f2609f2fde4fd3cc2764281b43ff24d40
Jair Sanchez <jair.sanchez@isas.de>
2025-06-30 15:37:37 +0200

    python>=3.10 support and workflows files updates

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""The setup script."""
 
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages 
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
 
 setup_requirements = [
     "pytest-runner>=5.2",
@@ -20,6 +17,7 @@ test_requirements = [
     "pytest>=5.4.3",
     "pytest-cov>=2.9.0",
     "pytest-raises>=0.11",
+    "numpy<2",
 ]
 
 dev_requirements = [
@@ -48,75 +46,10 @@ logger_requirements = [
     "tensorboard",
 ]
 
-requirements = [
-    "lightning==2.0.0",
-    "torch==2.0.1",
-    "monai>=1.2.0",
-    "aicsimageio==4.10.0",
-    "pandas",
-    "scikit-image",
-    "protobuf<4.21.0",
-    "pyrallis",
-    "scikit-learn",
-    "tensorboard",
-    "numba",
-]
 
-
-extra_requirements = {
-    "setup": setup_requirements,
-    "test": test_requirements,
-    "dev": dev_requirements,
-    "paper": [
-        *requirements,
-        *data_requirements,
-    ],
-    "advanced": [
-        *requirements,
-        *logger_requirements,
-    ],
-    "all": [
-        *requirements,
-        *logger_requirements,
-        *data_requirements,
-        *dev_requirements,
-    ]
-}
 
 setup(
-    author="Jianxu Chen",
-    author_email="jianxuchen.ai@gmail.com",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    description="A python package for deep learing based image to image transformation",  # noqa E501
-    entry_points={
-        "console_scripts": [
-            "run_im2im=mmv_im2im.bin.run_im2im:main"
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    include_package_data=True,
-    keywords="deep learning, microscopy image analysis, biomedical image analysis",
-    name="mmv_im2im",
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
-    python_requires=">=3.9",
-    setup_requires=setup_requirements,
-    test_suite="mmv_im2im/tests",
-    tests_require=test_requirements,
-    extras_require=extra_requirements,
-    url="https://github.com/MMV-Lab/mmv_im2im",
-    # Do not edit this string manually, always use bumpversion
-    # Details in CONTRIBUTING.rst
-    version="0.5.2",
-    zip_safe=False,
+    packages=["mmv_im2im"],
+    package_dir={"mmv_im2im": "mmv_im2im"},
+   
 )
