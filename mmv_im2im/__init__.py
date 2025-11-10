4557fd0154e3f5696fd78a8c79be962ed6df1198
Jianxu Chen <jxchen.ustc@gmail.com>
2025-11-10 20:07:42 +0100

    Bump version: 0.6.0 → 0.6.1

--- mmv_im2im/__init__.py
+++ mmv_im2im/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Jianxu Chen"
 __email__ = "jianxuchen.ai@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 
 def get_module_version():
