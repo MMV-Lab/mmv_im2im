78c4325f96e393e5d9a77d88f850920224083a26
Jair Sanchez <jair.sanchez@isas.de>
2025-11-13 17:36:09 +0100

    Preprocessing correction

--- mmv_im2im/__init__.py
+++ mmv_im2im/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Jianxu Chen"
 __email__ = "jianxuchen.ai@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.6.1"
+__version__ = "0.5.7"
 
 
 def get_module_version():
